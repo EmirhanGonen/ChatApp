e57ce45b99e36f5c7a7575be328aa18f8ebb0f5f
Emirhan Gönen <emirhangnn0607@gmail.com>
2024-12-01 13:34:11 +0300

    new: refactor classes
    Refactor classes and made connect disconnect and username

---
+++ ChatAppClient/ChatAppClient/chatClient.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+
+
+class chatClient
+{
+public:
+	chatClient() = default;
+
+	bool ConnectToServer();
+	void DisconnectToServer();
+
+	void SendMessageToServer(std::string message, const char message_buffer[]);
+
+
+	void SetUserName(const std::string new_user_name) { user_name = new_user_name; }
+	bool GetClientConnectedToServer() const { return connected_to_server; };
+	std::vector<std::string> GetClientMessages() const { return messages; }
+
+protected:
+	void ReceiveMessages();
+
+private:
+	void CreateReceiveChannel();
+	std::string ModifyMessage(const std::string& receivedMessage, const std::string& userName);
+
+	std::string user_name;
+	bool connected_to_server = false;
+	std::vector<std::string> messages;
+
+};
---
+++ ChatAppClient/ChatAppClient/clientWindow.cpp
@@ -0,0 +1,129 @@
+#include "clientWindow.h"
+#include "chatClient.h"
+#include <GLFW/glfw3.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+GLFWwindow* client_window;
+char client_message_buffer[256] = { 0 };
+
+
+GLFWwindow* clientWindow::Init(class chatClient* chat_client)
+{
+	if (!glfwInit())
+		return NULL;
+
+	client_window = glfwCreateWindow(640, 480, "ChatApp", NULL, NULL);
+	if (!client_window) {
+		glfwTerminate();
+		return NULL;
+	}
+
+	glfwMakeContextCurrent(client_window);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(client_window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+	this->chat_client = chat_client;
+
+	return client_window;
+}
+
+void clientWindow::StartTick()
+{
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+
+void clientWindow::Tick()
+{
+
+	ImGui::Begin("Chat App");
+
+	if (ImGui::Checkbox("Connect", &will_connect))
+	{
+		if (will_connect && !chat_client->GetClientConnectedToServer()) {
+			chat_client->ConnectToServer();
+		}
+		else if (chat_client->GetClientConnectedToServer())
+		{
+			chat_client->DisconnectToServer();
+		}
+	}
+
+	ImGui::SameLine();
+
+	bool client_connected = chat_client->GetClientConnectedToServer();
+	ImVec4 color = client_connected ? ImVec4(0.0f, 1.0f, 0.0f, 1.0f) : ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+	const char* text = client_connected ? "Connected" : "Disconnected";
+	ImGui::TextColored(color, "%s", text);
+
+	if (!client_connected) {
+		ImGui::End();
+		return;
+	}
+
+	if (!nickname_changed) {
+		ImGui::SetNextItemWidth(150.00f);
+
+		if (ImGui::InputText("NickName", nick_name_buffer, sizeof(nick_name_buffer), ImGuiInputTextFlags_EnterReturnsTrue)) {
+			chat_client->SetUserName(string(nick_name_buffer));
+			nickname_changed = true;
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("Change")) {
+			chat_client->SetUserName(string(nick_name_buffer));
+			nickname_changed = true;
+		}
+		ImGui::End();
+		return;
+	}
+
+	for (std::string& message : chat_client->GetClientMessages()) {
+		ImGui::Text(message.c_str());
+	}
+
+	if (ImGui::InputText("Message", client_message_buffer, sizeof(client_message_buffer), ImGuiInputTextFlags_EnterReturnsTrue))
+	{
+		string message(client_message_buffer);
+		chat_client->SendMessageToServer(message, client_message_buffer);
+
+		memset(client_message_buffer, 0, sizeof(client_message_buffer));
+	}
+	ImGui::End();
+}
+
+void clientWindow::EndTick()
+{
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+	//getline(cin, message);
+	//send(clientSocket, message.c_str(), message.length(), 0);
+
+	glfwSwapBuffers(client_window);
+	glfwPollEvents();
+}
+
+void clientWindow::Shutdown()
+{
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwTerminate();
+}
